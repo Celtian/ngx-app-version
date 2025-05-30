ddb05205f718a06a772c3b26ec9c68f93607d72b
Dominik Hladík <dominik.hladik@seznam.cz>
2025-05-30 23:47:49 +0200

    chore(deps): deps updated

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAppVersion } from 'ngx-app-version';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideAppVersion({
       version: '1.0.0'
     })
